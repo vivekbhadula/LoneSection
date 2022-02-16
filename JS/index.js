60af8ea93fbd0a2e0b5663c8ff5ba7bcf30b5fca
Vivek Bhadula <vivekbhadula201420@gmail.com>
2022-02-16 11:01:35 +0530

    complete layout

---
+++ JS/index.js
@@ -0,0 +1,9 @@
+function displayForm() {
+  document.getElementsByClassName("formContainer")[0].style.display = "block";
+  document.getElementsByClassName("darkScreen")[0].style.display = "block";
+}
+
+function closeForm() {
+  document.getElementsByClassName("formContainer")[0].style.display = "none";
+  document.getElementsByClassName("darkScreen")[0].style.display = "none";
+}
